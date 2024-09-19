a38162129e5b3683c5233a38e7ddea21421a2f51
fredle09 <le197903@gmail.com>
2024-09-19 19:47:20 +0700

    chores: update auth-router

--- demo-expo-stack-project/app/(auth)/_layout.tsx
+++ demo-expo-stack-project/app/(auth)/_layout.tsx
@@ -1,5 +1,13 @@
-import { Stack } from "expo-router"
+import { Redirect, Stack } from "expo-router"
+import { useContext } from "react";
+import { AuthContext } from "~/provider/auth-provider"
 
 export default function () {
+  const { user } = useContext(AuthContext);
+
+  if (user) {
+    return <Redirect href="/index" />;
+  }
+
   return <Stack />
 }
