ae38731378d22a6e363857a539313b5657b2f1b5
fredle09 <le197903@gmail.com>
2025-01-07 18:49:19 +0700

    chore(lab-5): justify filename

---
+++ lab-5/lib/icons/MapPinCheckInside.tsx
@@ -0,0 +1,5 @@
+import { MapPinCheckInside } from 'lucide-react-native';
+
+import { iconWithClassName } from './iconWithClassName';
+iconWithClassName(MapPinCheckInside);
+export { MapPinCheckInside };
